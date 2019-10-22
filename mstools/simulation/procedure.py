99ac25052a55e0606fd0464bdc03f849f10291a5
XiangYan <hnxxxy123@sjtu.edu.cn>
2019-10-22 19:05:51 +0800

    bug fix

--- mstools/simulation/procedure.py
+++ mstools/simulation/procedure.py
@@ -1,12 +1,13 @@
 class Procedure:
     NPT = 'npt'
+    NPT_MULTI = 'npt-multi'
     NVT_CV = 'nvt-cv'
     NVT_VISCOSITY = 'nvt-viscosity'
     NVT_VACUUM = 'nvt-vacuum'
     NVT_SLAB = 'nvt-slab'
     NPT_BINARY_SLAB = 'npt-binary-slab'
     NPT_PPM = 'ppm'
-    choices = [NPT, NVT_CV, NVT_VISCOSITY, NVT_VACUUM, NVT_SLAB, NPT_BINARY_SLAB, NPT_PPM]
+    choices = [NPT, NVT_CV, NVT_VISCOSITY, NVT_VACUUM, NVT_SLAB, NPT_BINARY_SLAB, NPT_PPM, NPT_MULTI]
 
     prior = {
         NVT_CV : NPT,
