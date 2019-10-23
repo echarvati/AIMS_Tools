6b5fc8bcace17c6a36ad476699e6060fc4054fbb
XiangYan <hnxxxy123@sjtu.edu.cn>
2019-10-23 15:33:54 +0800

    bug fix

--- mstools/analyzer/plot.py
+++ mstools/analyzer/plot.py
@@ -4,3 +4,6 @@ def plot(x_list, y_list):
     plt.plot(x_list, y_list)
     plt.show()
 
+job = Job.query.filter(Job.id==2).first()
+result = json.loads(job.result)
+plot(result['t_list'], result['vis_list'])
