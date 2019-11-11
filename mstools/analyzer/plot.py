716049166c265492b7e3733eae759dda3c914431
XiangYan <hnxxxy123@sjtu.edu.cn>
2019-11-11 13:38:34 +0800

    bug fix

--- mstools/analyzer/plot.py
+++ mstools/analyzer/plot.py
@@ -45,7 +45,7 @@ def gnuplot(output, xlabel, ylabel, title, txt_list=[], type_list=[], title_list
     f.write(info)
 
     silent = False
-    cmd = 'gnuplot < %s.gpi' % (output)
+    cmd = 'gnuplot %s.gpi' % (output)
     (stdout, stderr) = (PIPE, PIPE) if silent else (None, None)
     sp = Popen(cmd.split(), stdin=PIPE, stdout=stdout, stderr=stderr)
     sp.communicate()
