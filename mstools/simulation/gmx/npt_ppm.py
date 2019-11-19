508354e46c5988b7884ee8d9dc02826db2a485fb
XiangYan <hnxxxy123@sjtu.edu.cn>
2019-11-19 20:36:58 +0800

    bug fix

--- mstools/simulation/gmx/npt_ppm.py
+++ mstools/simulation/gmx/npt_ppm.py
@@ -421,8 +421,8 @@ class NptPPM(GmxSimulation):
     def get_post_data(post_result, T, P, smiles_list, **kwargs) -> dict:
         from ...analyzer.fitting import VTFval
         converge_criterion = 0.95  # R value of fitting
-        if len(post_result.get('vis-t-VFT'))==1:
-            coef, score, tmin, tmax = post_result['vis-t-VFT'][str(P)]
+        if len(post_result.get('vis-t-VTF'))==1:
+            coef, score, tmin, tmax = post_result['vis-t-VTF'][str(P)]
             if not (score < converge_criterion or T < tmin - 10 or T > tmax + 10):
                 vis = VTFval(T, coef)
             else:
