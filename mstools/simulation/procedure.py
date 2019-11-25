e0dd1df4358c9b16970123d5dcba1af0e010c1fb
XiangyanSJTU <hnxxxy123@sjtu.edu.cn>
2019-11-26 01:13:56 +0800

    update

--- mstools/simulation/procedure.py
+++ mstools/simulation/procedure.py
@@ -9,16 +9,20 @@ class Procedure:
     NVT_SLAB = 'nvt-slab'
     NPT_BINARY_SLAB = 'npt-binary-slab'
     NPT_PPM = 'ppm'
-    NPT_BIG = 'npt-big'
-    NVT_MULTI_BIG = 'nvt-multi-big'
+    NPT_2 = 'npt-2'
+    NPT_3 = 'npt-2'
+    NVT_MULTI_2 = 'nvt-multi-2'
+    NVT_MULTI_3 = 'nvt-multi-3'
     choices = [NPT, NVT_CV, NVT_VISCOSITY, NVT_VACUUM, NVT_SLAB, NPT_BINARY_SLAB, NPT_PPM, NPT_MULTI, NVT_MULTI,
-               NPT_V_RESCALE, NPT_BIG, NVT_MULTI_BIG]
+               NPT_V_RESCALE, NPT_2, NPT_3, NVT_MULTI_2, NVT_MULTI_3]
 
     prior = {
         NVT_CV: NPT,
         NVT_VISCOSITY: NPT,
         NPT_PPM: NPT,
         NVT_MULTI: NPT,
-        NPT_BIG: NPT,
-        NVT_MULTI_BIG: NPT_BIG
+        NPT_2: NPT,
+        NPT_3: NPT,
+        NVT_MULTI_2: NPT_2,
+        NVT_MULTI_3: NPT_3,
     }
